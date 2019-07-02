241c6ada1203939e2de60b652af7c4cb8379f601
PaulTmatik <t.matik1@gmail.com>
2019-07-02 12:46:01 +0700

    удаление ненужного аргумента

--- src/actions/groupsActions.js
+++ src/actions/groupsActions.js
@@ -1,11 +1,10 @@
 import { ACTIONS_NAMES } from ".";
 
-export const getGroupsOnPeriiod = (period, groups) => {
+export const getGroupsOnPeriod = period => {
   return {
     type: ACTIONS_NAMES.GROUPS_GET_ON_PERIOD,
     payload: {
-      period,
-      groups
+      period
     }
   }
 }
