91ee4d075b27011fcd94c249c377747fd47fc625
PaulTmatik <t.matik1@gmail.com>
2019-07-02 23:06:24 +0700

    Добавлена функция морфинга слов

---
+++ src/helpers/pluralize.js
@@ -0,0 +1,12 @@
+export const pluralize = (number, variants) => {
+  const vars = variants.split("|");
+  let units = number % 100;
+  units = units < 20 ? units : units % 10;
+  if (vars.length >= 1 && units === 1)
+    return `${number} ${vars[0]}`;
+  if (vars.length >= 2 && (units > 1 && units < 5))
+    return `${number} ${vars[1]}`;
+  if (vars.length === 3 && (units > 4 || units === 0))
+    return `${number} ${vars[2]}`;
+  return `${number} ${variants}`;
+}
---
+++ src/helpers/pluralize.test.js
@@ -0,0 +1,21 @@
+import {pluralize} from ".";
+const studentWord = "студент|студента|студентов";
+it("Для значения 1, слово студент должно быть \"студент\"", () => {
+    expect(pluralize(1, studentWord)).toEqual("1 студент");
+});
+
+it("Для значения 2, слово студент должно быть \"студента\"", () => {
+    expect(pluralize(2, studentWord)).toEqual("2 студента");
+});
+
+it("Для значения 5, слово студент должно быть \"студентов\"", () => {
+    expect(pluralize(5, studentWord)).toEqual("5 студентов");
+});
+
+it("Для значения 0, слово студент должно быть \"студентов\"", () => {
+    expect(pluralize(0, studentWord)).toEqual("0 студентов");
+});
+
+it("Для значения 101, слово студент должно быть \"студент\"", () => {
+    expect(pluralize(101, studentWord)).toEqual("101 студент");
+});
