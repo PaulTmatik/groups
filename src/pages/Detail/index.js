a853d82768da01f031f8ee2cee9806df8df33a03
PaulTmatik <t.matik1@gmail.com>
2019-07-04 19:57:01 +0700

    Добавлено отображение имени группы

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,14 +1,24 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class GroupDetail extends Component {
   render() {
-    const { group } = this.props;
+    const { group, period } = this.props;
+    const originalGroupName = !period.isCurrent()
+      ? <div>Текущее имя группы {group.getNameFromDate(new Date())}</div> 
+      : null;
     return (
       <div className="page">
+        <h1>{group.getNameFromDate(period.end)}</h1>
+        {originalGroupName}
         {group.guid}
       </div>
     )
   }
 }
 
-export default GroupDetail;
+const mapStateToProps = state => ({
+  period: state.period
+});
+
+export default connect(mapStateToProps)(GroupDetail);
